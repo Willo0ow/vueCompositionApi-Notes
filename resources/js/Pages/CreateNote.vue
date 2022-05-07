3a7b63126def67fd20af5b644c166cc79f6d7954
Willo0ow <dhinc39@gmail.com>
2022-05-07 18:52:38 +0200

    overwrite created at for a new note, ignore public files

--- resources/js/Pages/CreateNote.vue
+++ resources/js/Pages/CreateNote.vue
@@ -10,17 +10,34 @@
         v-model="content"
         label="Treść"
       />
-      <v-autocomplete
-        v-if="categories.length"
-        v-model="category"
-        :items="categories"
-        @update:modelValue="getCategorySubs"
-      />
-      <v-autocomplete
-        v-if="subCategories.length"
-        v-model="subCategory"
-        :items="subCategories"
-      />
+      <v-container fluid>
+        <v-row fluid>
+          <v-col>
+            <v-autocomplete
+              v-if="categories.length"
+              v-model="category"
+              :items="categories"
+              label="Kategoria"
+              @update:modelValue="getCategorySubs"
+            />
+          </v-col>
+          <v-col>
+            <v-autocomplete
+              v-if="subCategories.length"
+              v-model="subCategory"
+              :items="subCategories"
+              label="Podkategoria"
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="created_at"
+              type="datetime-local"
+              label="Data utworzenia"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card-text>
     <v-card-actions>
       <v-btn
@@ -45,9 +62,12 @@ const category = ref(null);
 const subCategory = ref(null);
 const categories = ref([]);
 const subCategories = ref([]);
+const created_at = ref("");
+
 const reset = () => {
   subject.value = "";
   content.value = "";
+  created_at.value = "";
   category.value = null;
   subCategory.value = null;
 }
@@ -57,9 +77,16 @@ const getCategorySubs= async () => {
 }
 
 const save = async () =>{
-  await axios.post("/api/note", 
-  {subject: subject.value, content: content.value, category: category.value, subCategory: subCategory.value}
-  )
+  const form = {
+      subject: subject.value, 
+      content: content.value, 
+      category: category.value, 
+      subCategory: subCategory.value, 
+    }
+    if(created_at.value){
+      form.created_at = created_at.value;
+    }
+  await axios.post("/api/note",form)
   reset();
 }
 
